94e9023454c63b83e83010f3bb2a1c5cfab15856
ashishb_sdet <ashishb.sdet@gmail.com>
2026-09-12 13:32:22 -0700

    Run the Cucumber scenarios with TestNG

--- src/test/java/dev/ashish/qa/runner/RunCucumberTest.java
+++ src/test/java/dev/ashish/qa/runner/RunCucumberTest.java
@@ -1,15 +1,22 @@
 package dev.ashish.qa.runner;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "dev.ashish.qa")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report.html, json:target/cucumber.json")
-public class RunCucumberTest {}
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "dev.ashish.qa",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report.html",
+                "json:target/cucumber.json"
+        }
+)
+public class RunCucumberTest extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
